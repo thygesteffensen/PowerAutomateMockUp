7d4132b7eb917c6151fd8c02df1b1a8f131ccdc5
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-11-25 16:48:47 +0100

    Changed the path to use flow file in bin dir

--- Test/FullFlowTest.cs
+++ Test/FullFlowTest.cs
@@ -14,7 +14,7 @@ namespace Test
     [TestFixture]
     public class FullFlowTest
     {
-        private static readonly string TestFlowPath = System.IO.Path.GetFullPath(@"..\..\..\FlowSamples");
+        private static readonly string TestFlowPath = System.IO.Path.GetFullPath(@"FlowSamples");
 
         [Test]
         public async Task TestFlowFalse()
