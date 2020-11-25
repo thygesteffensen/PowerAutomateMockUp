f798dace3764409c52436733f7832cc5636ff165
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-11-25 14:33:04 +0100

    fix: Full flow test

--- Test/FullFlowTest.cs
+++ Test/FullFlowTest.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
