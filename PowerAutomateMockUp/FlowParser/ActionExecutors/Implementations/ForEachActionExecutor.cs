6fc10958e3808e3c479defee64bcdc884e40c978
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-12-28 20:59:11 +0100

    refactor: Refactored ForEachActionExecutor.cs

--- PowerAutomateMockUp/FlowParser/ActionExecutors/Implementations/ForEachActionExecutor.cs
+++ PowerAutomateMockUp/FlowParser/ActionExecutors/Implementations/ForEachActionExecutor.cs
@@ -15,6 +15,9 @@ namespace Parser.FlowParser.ActionExecutors.Implementations
         private readonly ILogger<ForEachActionExecutor> _logger;
         private readonly IExpressionEngine _expressionEngine;
 
+        private JProperty[] _actionDescriptions;
+        private string _firstScopeActionName;
+
         private List<ValueContainer> _items;
 
         public ForEachActionExecutor(
@@ -35,7 +38,13 @@ namespace Parser.FlowParser.ActionExecutors.Implementations
         {
             _logger.LogInformation("Entered foreach...");
 
-            var runOn = Json.SelectToken("$..foreach").Value<string>();
+            if (Json == null)
+            {
+                throw new PowerAutomateMockUpException($"Json cannot be null - cannot execute {ActionName}.");
+            }
+
+            var runOn = (Json.SelectToken("$..foreach") ??
+                         throw new InvalidOperationException("Json must contain foreach token.")).Value<string>();
             var values = _expressionEngine.ParseToValueContainer(runOn);
 
             if (values.Type() != ValueContainer.ValueType.Array)
@@ -44,28 +53,36 @@ namespace Parser.FlowParser.ActionExecutors.Implementations
                     // TODO: Figure out what happens when you apply for each on non array values
                     ActionStatus = ActionStatus.Failed
                 });
-            
-            var valueList = values.GetValue<IEnumerable<ValueContainer>>().ToArray();
-                
-            var firstScopeAction = SetupScope();
+
+            SetupForEach(values);
+
+            UpdateScopeAndSetItemValue();
+
+            return Task.FromResult(new ActionResult {NextAction = _firstScopeActionName});
+        }
+
+        private void SetupForEach(ValueContainer values)
+        {
+            var scopeActionDescriptions = (Json.SelectToken("$.actions") ??
+                                           throw new InvalidOperationException("Json must contain actions token."))
+                .OfType<JProperty>();
+            _actionDescriptions = scopeActionDescriptions as JProperty[] ?? scopeActionDescriptions.ToArray();
+            _firstScopeActionName = _actionDescriptions.First(ad =>
+                !(ad.Value.SelectToken("$.runAfter") ??
+                  throw new InvalidOperationException("Json must contain runAfter token.")).Any()).Name;
 
             // TODO: Add scope relevant storage to store stuff like this, which cannot interfere with the state.
-            _state.AddOutputs($"item_{ActionName}", valueList.First());
-            _items = valueList.Skip(1).ToList();
-
-            return Task.FromResult(new ActionResult {NextAction = firstScopeAction.Name});
+            _items = values.GetValue<IEnumerable<ValueContainer>>().ToList();
         }
 
-        private JProperty SetupScope()
+        private void UpdateScopeAndSetItemValue()
         {
-            var scopeActionDescriptions = Json.SelectToken("$.actions").OfType<JProperty>();
-            var actionDescriptions = scopeActionDescriptions as JProperty[] ?? scopeActionDescriptions.ToArray();
+            _scopeDepthManager.Push(ActionName, _actionDescriptions, this);
 
-            _scopeDepthManager.Push(ActionName, actionDescriptions, this);
-
-            var firstScopeAction = actionDescriptions.First(ad => !ad.Value.SelectToken("$.runAfter").Any());
-            return firstScopeAction;
+            _state.AddOutputs($"item_{ActionName}", _items.First());
+            _items = _items.Skip(1).ToList();
         }
+
 
         public Task<ActionResult> ExitScope(ActionStatus scopeStatus)
         {
@@ -73,13 +90,9 @@ namespace Parser.FlowParser.ActionExecutors.Implementations
             {
                 _logger.LogInformation("Continuing foreach.");
 
-                _state.AddOutputs($"item_{ActionName}", _items.First());
+                UpdateScopeAndSetItemValue();
 
-                _items = _items.Skip(1).ToList();
-                
-                var firstScopeAction = SetupScope();
-
-                return Task.FromResult(new ActionResult {NextAction = firstScopeAction.Name});
+                return Task.FromResult(new ActionResult {NextAction = _firstScopeActionName});
             }
             else
             {
