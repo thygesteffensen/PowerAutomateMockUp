ebe242e217140637bec6ae5cfffe4524a089bb8f
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-06 12:42:16 +0100

    refactor: Expression test

---
+++ Test/Expression/GenericExceptionTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Parser.ExpressionParser;
+using Parser.ExpressionParser.Functions.Base;
+
+namespace Test.Expression
+{
+    [TestFixture]
+    public class GenericExceptionTest
+    {
+        [Test,TestCaseSource(typeof(StringFunctionTests), nameof(StringFunctionTests.StringFunctionTestInput))]
+        [TestCaseSource(typeof(CollectionFunctionTests), nameof(CollectionFunctionTests.CollectionFunctionTestInput))]
+        public void TestFunctions(Function func, string name, ValueContainer[] parameters,
+            ValueContainer expected)
+        {
+            Assert.AreEqual(name, func.FunctionName);
+
+            var result = func.ExecuteFunction(parameters);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
