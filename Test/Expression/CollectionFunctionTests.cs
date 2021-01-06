dfc120f2403ee330c4c37428e711411751b2deca
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-06 12:42:20 +0100

    feat: Collections - Added contains
    #3

---
+++ Test/Expression/CollectionFunctionTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Parser.ExpressionParser;
+using Parser.ExpressionParser.Functions.Implementations.CollectionFunctions;
+
+namespace Test.Expression
+{
+    public class CollectionFunctionTests
+    {
+        internal static object[] CollectionFunctionTestInput =
+        {
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[] {new ValueContainer("Hello world!"), new ValueContainer("world")},
+                new ValueContainer(true)
+            },
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[] {new ValueContainer("Hello world!"), new ValueContainer("univers")},
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[]
+                {
+                    new ValueContainer(new[] {new ValueContainer(1), new ValueContainer(2), new ValueContainer(3)}),
+                    new ValueContainer(1)
+                },
+                new ValueContainer(true)
+            },
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[]
+                {
+                    new ValueContainer(new[] {new ValueContainer(1), new ValueContainer(2), new ValueContainer(3)}),
+                    new ValueContainer(4)
+                },
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[]
+                {
+                    new ValueContainer(new Dictionary<string, ValueContainer>
+                        {{"key1", new ValueContainer()}, {"key2", new ValueContainer()}}),
+                    new ValueContainer("key2")
+                },
+                new ValueContainer(true)
+            },
+            new object[]
+            {
+                new ContainsFunction(),
+                "contains",
+                new[]
+                {
+                    new ValueContainer(new Dictionary<string, ValueContainer>
+                        {{"key1", new ValueContainer()}, {"key2", new ValueContainer()}}),
+                    new ValueContainer("key3")
+                },
+                new ValueContainer(false)
+            },
+        };
+    }
+}
