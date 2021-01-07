bdf4b0012e2447b568d81da51e28863bcd3b19c7
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-07 16:14:42 +0100

    test: Collection function tests
    #3

--- Test/Expression/CollectionFunctionTests.cs
+++ Test/Expression/CollectionFunctionTests.cs
@@ -68,6 +68,109 @@ namespace Test.Expression
                 },
                 new ValueContainer(false)
             },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer("key3")
+                },
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer("key3")
+                },
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer("")
+                },
+                new ValueContainer(true)
+            },
+            
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer()
+                },
+                new ValueContainer(true)
+            },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer(new []{new ValueContainer("Item")})
+                },
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer(new Dictionary<string, ValueContainer>())
+                },
+                new ValueContainer(true)
+            },
+            new object[]
+            {
+                new EmptyFunction(),
+                "empty",
+                new[]
+                {
+                    new ValueContainer(new Dictionary<string, ValueContainer> {{"Key", new ValueContainer("value")}})
+                },
+                new ValueContainer(false)
+            },
+            new object[]
+            {
+                new FirstFunction(),
+                "first",
+                new[]
+                {
+                    new ValueContainer("1234")
+                },
+                new ValueContainer("1")
+            },
+            new object[]
+            {
+                new FirstFunction(),
+                "first",
+                new[]
+                {
+                    new ValueContainer(new []{new ValueContainer("first"), new ValueContainer("second")})
+                },
+                new ValueContainer("first")
+            },
+            new object[]
+            {
+                new InterSectionFunction(),
+                "intersection",
+                new[]
+                {
+                    new ValueContainer(new []{new ValueContainer("first"), new ValueContainer("second")}),
+                    new ValueContainer(new []{new ValueContainer("second"), new ValueContainer("third")}),
+                },
+                new ValueContainer(new []{new ValueContainer("second")})
+            },
         };
     }
+    
 }
