eb3c537a1871341bf17a5af5348515dd06c2bb4e
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-08 09:26:21 +0100

    feat: Implemented the Collection Expression: Length
    #3

---
+++ PowerAutomateMockUp/ExpressionParser/Functions/Implementations/CollectionFunctions/LengthFunction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Parser.ExpressionParser.Functions.Base;
+
+namespace Parser.ExpressionParser.Functions.Implementations.CollectionFunctions
+{
+    public class LengthFunction : Function
+    {
+        public LengthFunction() : base("length")
+        {
+        }
+
+        public override ValueContainer ExecuteFunction(params ValueContainer[] parameters)
+        {
+            var value = parameters[0];
+
+            return value.Type() switch
+            {
+                ValueContainer.ValueType.String => new ValueContainer(
+                    value.GetValue<string>().Length),
+                ValueContainer.ValueType.Array => new ValueContainer(
+                    value.GetValue<IEnumerable<ValueContainer>>().Count()),
+                _ => throw new PowerAutomateMockUpException(
+                    $"Empty expression can only operate on String or Array types, not {value.Type()}.")
+            };
+        }
+    }
+}
