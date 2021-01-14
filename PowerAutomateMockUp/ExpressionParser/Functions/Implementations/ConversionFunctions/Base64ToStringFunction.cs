19a3b7d576d3a0812d876dcbd3d4f0c9e549f093
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-14 16:24:48 +0100

    feat: Some data functions

---
+++ PowerAutomateMockUp/ExpressionParser/Functions/Implementations/ConversionFunctions/Base64ToStringFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Parser.ExpressionParser.Functions.Base;
+
+namespace Parser.ExpressionParser.Functions.Implementations.ConversionFunctions
+{
+    public class Base64ToStringFunction : Function
+    {
+        public Base64ToStringFunction() : base("base64ToString")
+        {
+        }
+
+        public override ValueContainer ExecuteFunction(params ValueContainer[] parameters)
+        {
+            return parameters[0].Type() switch
+            {
+                ValueContainer.ValueType.String =>
+                    new ValueContainer(Encoding.UTF8.GetString(Convert.FromBase64String(parameters[0].GetValue<string>()))),
+                _ => throw new PowerAutomateMockUpException(
+                    $"Array function can only operate on strings, not {parameters[0].Type()}.")
+            };
+        }
+    }
+}
---
+++ PowerAutomateMockUp/ExpressionParser/Functions/Implementations/ConversionFunctions/DataUriFunction.cs
@@ -0,0 +1,23 @@
+﻿using Parser.ExpressionParser.Functions.Base;
+
+namespace Parser.ExpressionParser.Functions.Implementations.ConversionFunctions
+{
+    public class DataUriFunction : Function
+    {
+        public DataUriFunction() : base("dataUri")
+        {
+        }
+
+        public override ValueContainer ExecuteFunction(params ValueContainer[] parameters)
+        {
+            return parameters[0].Type() switch
+            {
+                ValueContainer.ValueType.String =>
+                    new ValueContainer(
+                        $"data:text/plain;charset=utf-8;base64,{System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(parameters[0].GetValue<string>()))}"),
+                _ => throw new PowerAutomateMockUpException(
+                    $"Array function can only operate on strings, not {parameters[0].Type()}.")
+            };
+        }
+    }
+}
