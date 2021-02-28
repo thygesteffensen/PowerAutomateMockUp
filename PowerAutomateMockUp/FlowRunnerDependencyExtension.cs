c0e71a3d7846219c2cbf7afe6cb486dc3bd974b6
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-02-28 13:01:15 +0100

    test: Added trigger by value container

--- PowerAutomateMockUp/FlowRunnerDependencyExtension.cs
+++ PowerAutomateMockUp/FlowRunnerDependencyExtension.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Parser.ExpressionParser;
 using Parser.ExpressionParser.Functions.Base;
 using Parser.ExpressionParser.Functions.Implementations.CollectionFunctions;
+using Parser.ExpressionParser.Functions.Implementations.ConversionFunctions;
 using Parser.ExpressionParser.Functions.Implementations.LogicalComparisonFunctions;
 using Parser.ExpressionParser.Functions.Implementations.StringFunctions;
 using Parser.ExpressionParser.Functions.Storage;
 using Parser.FlowParser;
 using Parser.FlowParser.ActionExecutors;
 using Parser.FlowParser.ActionExecutors.Implementations;
+using Parser.FlowParser.ActionExecutors.Implementations.ControlActions;
 using LengthFunction = Parser.ExpressionParser.Functions.Implementations.StringFunctions.LengthFunction;
 
 namespace Parser
@@ -31,6 +34,7 @@ namespace Parser
 
             AddStringFunctions(services);
             AddCollectionFunction(services);
+            AddConversionFunction(services);
 
             services.AddTransient<IFunction, VariablesFunction>();
             services.AddTransient<IFunction, OutputsFunction>();
@@ -80,6 +84,19 @@ namespace Parser
             services.AddTransient<IFunction, SkipFunction>();
             services.AddTransient<IFunction, TakeFunction>();
             services.AddTransient<IFunction, UnionFunction>();
+        }
+
+        private static void AddConversionFunction(IServiceCollection services)
+        {
+            services.AddTransient<IFunction, ArrayFunction>();
+            services.AddTransient<IFunction, Base64Function>();
+            services.AddTransient<IFunction, Base64ToBinaryFunction>();
+            services.AddTransient<IFunction, Base64ToStringFunction>();
+            services.AddTransient<IFunction, BinaryFunction>();
+            services.AddTransient<IFunction, BoolFunction>();
+            services.AddTransient<IFunction, CreateArrayFunction>();
+            services.AddTransient<IFunction, DataUriFunction>();
+            services.AddTransient<IFunction, DataUriToBinaryFunction>();
         }
 
         public static void AddFlowActionByName<T>(this IServiceCollection services, string actionName)
---
+++ Test/TestValueContainerTrigger.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Parser;
+using Parser.ExpressionParser;
+using Parser.FlowParser;
+using Parser.FlowParser.ActionExecutors;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestValueContainerTrigger
+    {
+        private static readonly string TestFlowPath = System.IO.Path.GetFullPath(@"FlowSamples");
+
+        [Test]
+        public async Task TestFlowFalse()
+        {
+            // Arrange
+            var path = @$"{TestFlowPath}\ContactTrigger.json";
+
+            var services = new ServiceCollection();
+
+            services.AddFlowActionByName<UpdateRowActionExecutor>(UpdateRowActionExecutor.FlowActionName);
+            services.AddFlowActionByName<SendNotification>(SendNotification.FlowActionName);
+
+            services.AddFlowRunner();
+
+            var sp = services.BuildServiceProvider();
+            var flowRunner = sp.GetRequiredService<IFlowRunner>();
+
+            flowRunner.InitializeFlowRunner(path);
+
+            var triggerOutput = new ValueContainer(new Dictionary<string, ValueContainer>
+            {
+                {
+                    "body", new ValueContainer(new Dictionary<string, ValueContainer>
+                    {
+                        {"firstname", new ValueContainer("John")},
+                        // {"lastname", new ValueContainer("Doe")}
+                    })
+                }
+            });
+
+            // Act
+            await flowRunner.Trigger(triggerOutput);
+
+            // Assert
+            var state = sp.GetRequiredService<IState>();
+
+            Assert.IsTrue(state.GetOutputs(SendNotification.FlowActionName).Type() != ValueContainer.ValueType.Null);
+            Assert.IsTrue(state.GetOutputs(UpdateRowActionExecutor.FlowActionName).Type() == ValueContainer.ValueType.Null);
+
+            Assert.IsTrue(state.GetOutputs(SendNotification.FlowActionName).GetValue<bool>(),
+                "Second action wasn't triggered");
+        }
+    }
+
+    public class UpdateRowActionExecutor : OpenApiConnectionActionExecutorBase
+    {
+        public static string FlowActionName = "Create_a_new_row_-_Create_greeting_note";
+
+        public UpdateRowActionExecutor(IExpressionEngine expressionEngine) : base(expressionEngine)
+        {
+        }
+
+        public override Task<ActionResult> Execute()
+        {
+            return Task.FromResult(new ActionResult
+            {
+                ActionOutput = new ValueContainer(true)
+            });
+        }
+    }
+    
+    public class SendNotification : OpenApiConnectionActionExecutorBase
+    {
+        public static string FlowActionName = "Send_me_an_email_notification";
+        
+        public SendNotification(IExpressionEngine expressionEngine) : base(expressionEngine)
+        {
+        }
+
+        public override Task<ActionResult> Execute()
+        {
+            return Task.FromResult(new ActionResult
+            {
+                ActionOutput = new ValueContainer(true)
+            });
+        }
+    }
+}
