fa18aa5afd390b863eb2847b077a971854f25391
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-02-28 12:59:35 +0100

    feat: added create array function

---
+++ PowerAutomateMockUp/ExpressionParser/Functions/Implementations/ConversionFunctions/CreateArrayFunction.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Parser.ExpressionParser.Functions.Base;
+using Parser.ExpressionParser.Functions.CustomException;
+
+namespace Parser.ExpressionParser.Functions.Implementations.ConversionFunctions
+{
+    public class CreateArrayFunction : Function
+    {
+        public CreateArrayFunction() : base("createArray")
+        {
+        }
+
+        public override ValueContainer ExecuteFunction(params ValueContainer[] parameters)
+        {
+            if (parameters.Length == 0)
+            {
+                throw InvalidTemplateException.BuildInvalidLanguageFunction("SomeActon", "createArray");
+            }
+
+            return new ValueContainer(parameters.ToList());
+        }
+    }
+}
