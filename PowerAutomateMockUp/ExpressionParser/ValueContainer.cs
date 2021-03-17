54c610e76d0a79a9e8df6dd2d0bc164ec0c352f6
Thyge Steffensen <s175176@student.dtu.dk>
2021-03-17 11:06:18 +0100

    feat: Added ContainsKey function to traverse value container

--- PowerAutomateMockUp/ExpressionParser/ValueContainer.cs
+++ PowerAutomateMockUp/ExpressionParser/ValueContainer.cs
@@ -432,6 +432,26 @@ namespace Parser.ExpressionParser
         {
             return new {_type, _value}.GetHashCode();
         }
+
+        public bool ContainsKey(string key)
+        {
+            if (_type != ValueType.Object) return false;
+
+            var current = AsDict();
+            foreach (var k in key.Split('/'))
+            {
+                if (current.ContainsKey(k))
+                {
+                    current = current[k].AsDict();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     public class ValueContainerComparer : EqualityComparer<ValueContainer>
@@ -449,65 +469,6 @@ namespace Parser.ExpressionParser
         public override int GetHashCode(ValueContainer obj)
         {
             return obj.GetHashCode();
-        }
-    }
-
-    static class ValueContainerExtensions
-    {
-        public static Dictionary<string, ValueContainer> Normalize(this Dictionary<string, ValueContainer> input)
-        {
-            var temp = new Dictionary<string, ValueContainer>();
-
-            foreach (var keyValuePair in input)
-            {
-                var keys = keyValuePair.Key.Split('/');
-
-                BuildNest(keys, keyValuePair.Value, new ValueContainer(temp, false));
-            }
-
-            return temp;
-        }
-
-        private static ValueContainer BuildNest(string[] keys, ValueContainer value, ValueContainer current)
-        {
-            if (keys.Length == 1)
-            {
-                var dict = current.GetValue<Dictionary<string, ValueContainer>>();
-                if (dict.ContainsKey(keys[0]) && value.Type() == ValueContainer.ValueType.Object)
-                {
-                    var innerDict = dict[keys[0]].GetValue<Dictionary<string, ValueContainer>>();
-                    var valueDict = value.GetValue<Dictionary<string, ValueContainer>>();
-                    foreach (var keyValuePair in valueDict)
-                    {
-                        innerDict[keyValuePair.Key] = keyValuePair.Value;
-                    }
-                }
-                else
-                {
-                    dict[keys[0]] = value;
-                }
-
-                return new ValueContainer(dict, false);
-            }
-            else
-            {
-                var dict = current.GetValue<Dictionary<string, ValueContainer>>();
-
-                if (dict.ContainsKey(keys.First()))
-                {
-                    var innerDict = dict[keys.First()].GetValue<Dictionary<string, ValueContainer>>();
-
-                    BuildNest(keys.Skip(1).ToArray(), value, new ValueContainer(innerDict, false));
-                }
-                else
-                {
-                    var t = BuildNest(keys.Skip(1).ToArray(), value,
-                        new ValueContainer(new Dictionary<string, ValueContainer>(), false));
-                    dict[keys.First()] = t;
-                }
-
-                return new ValueContainer(dict, false);
-            }
         }
     }
 }
